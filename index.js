20c9acfff38c5fcbd31f23d0dfc444dafe4389b8
Matias <hmatiasrearte@gmail.com>
2023-11-16 23:20:50 -0300

    add js

--- index.js
+++ index.js
@@ -36,3 +36,36 @@ const prevPortada = () => {
 nextBtn.addEventListener("click", nextPortada);
 prevBtn.addEventListener("click", prevPortada);
 
+// -----------------volumenLoco-------------------
+
+animate(document.getElementById('bar1'), [350, 250, 310, 150]);
+                animate(document.getElementById('bar2'), [50, 150, 100, 350]);
+                animate(document.getElementById('bar3'), [300, 200, 250, 100]);
+                animate(document.getElementById('bar4'), [375, 145, 215, 95]);
+                animate(document.getElementById('bar5'), [75, 115, 80, 235]);
+                animate(document.getElementById('bar6'), [175, 45, 195, 115]);
+                animate(document.getElementById('bar7'), [215, 145, 300, 95]);
+                animate(document.getElementById('bar8'), [55, 345, 145, 250]);
+
+                function animate(element, heights) {
+                    let currentHeight = 0;
+                    let loop = 0;
+
+                    setInterval(function() {
+                        if (currentHeight === heights[loop]) {
+                            loop++;
+
+                            if (!heights[loop]) {
+                                loop = 0;
+                            }
+                        } else {
+                            if (currentHeight > heights[loop]) {
+                                currentHeight--;
+                            } else {
+                                currentHeight++;
+                            }
+
+                            element.style.height = currentHeight + 'px';
+                        }
+                    }, 5);
+                }
---
+++ products.js
@@ -0,0 +1,182 @@
+const infoProductos = [
+    {
+    id: 1,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 2,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 3,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 4,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 5,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 6,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 7,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 8,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 9,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 10,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 11,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 12,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 13,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 14,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 15,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 16,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 17,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 18,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 19,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+{
+    id: 20,
+    precio:,
+    nombre:,
+    banda:,
+    genero:,
+    categoria:,
+    imgTapa:,
+}
+]
